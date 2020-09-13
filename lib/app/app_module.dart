325c27d195f9f7fe8a2de9602292bc0dee26b931
Renan <renan_sacavalcante@hotmail.com>
2020-09-13 12:23:16 -0300

    HomePage and OtherPage

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:slidy_modular/app/app_widget.dart';
+import 'package:slidy_modular/app/pages/home/home_controller.dart';
+import 'package:slidy_modular/app/pages/home/home_page.dart';
+import 'package:slidy_modular/app/pages/other/other_page.dart';
+
+import 'app_controller.dart';
 
 class AppModule extends MainModule {
   @override
-  List<Bind> get binds => throw UnimplementedError();
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+        Bind((i) => HomeController()),
+      ];
 
   @override
   List<ModularRouter> get routers => [
+        //HomePage
         ModularRouter(
           '/',
-          child: (_, args) => Container(),
+          child: (_, args) => HomePage(),
         ),
+        //OtherPage
+        ModularRouter(
+          '/other/:text',
+          child: (_, args) => OtherPage(
+            text: args.params['text'],
+          ),
+        )
       ];
 
   @override
